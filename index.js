4f0483a7e358c5afaec582e311601a01a62f4157
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-12 14:32:37 -0300

    Creates html index page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('./src/index.html')
 })
 
 app.listen(50000, () => console.log('Server running on port 3000'))
