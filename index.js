f5310b8b96c8d21770f9d14e51f7c65b3ed8b98d
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-13 11:17:59 -0300

    Creates a public folder for send static files

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(50000, () => console.log('Server running on port 50000'))
